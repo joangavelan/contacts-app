a653db78e32ed48991a3acdbb854582f3d86f412
joangavelan <joangavelan@gmail.com>
2024-05-15 16:10:58 -0500

    feat: add header and footer layouts to auth pages

--- handler/sign-in.go
+++ handler/sign-in.go
@@ -9,6 +9,8 @@ func SignIn(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles(
 		"web/templates/base.html",
 		"web/templates/auth/layout.html",
+		"web/templates/commons/header.html",
+		"web/templates/commons/footer.html",
 		"web/templates/auth/sign-in.html",
 	))
 	
