d6a9b49f5bd834221fcab105b32fa8086777df9b
joangavelan <joangavelan@gmail.com>
2024-05-25 21:37:21 -0500

    refactor: enhance removal of submit button text during form requests

---
+++ web/static/js/forms.js
@@ -0,0 +1,19 @@
+// Remove the text from submit buttons to show only the spinner during form requests
+document.addEventListener('htmx:beforeRequest', function (e) {
+  const t = getSubmitButtonText(e)
+  t.style.display = 'none'
+})
+
+document.addEventListener('htmx:afterRequest', function (e) {
+  const t = getSubmitButtonText(e)
+  t.style.display = 'block'
+})
+
+function getSubmitButtonText(e) {
+  const dispatcherEl = e.detail.elt
+  if (dispatcherEl.tagName === 'FORM') {
+    const submitButton = dispatcherEl.querySelector('button[type="submit"]')
+    const submitButtonText = submitButton.querySelector('p')
+    return submitButtonText
+  }
+}
--- web/static/js/script.js
+++
@@ -1,9 +0,0 @@
-// Remove the text from submit buttons to show only the spinner during form requests
-document.addEventListener("htmx:beforeRequest", function () {
-  const button = document.querySelector('button[type="submit"]')
-  button.childNodes.forEach((child) => {
-    if (child.nodeType === Node.TEXT_NODE && child.textContent.trim() !== "") {
-      child.textContent = "" // Remove the text
-    }
-  })
-})
